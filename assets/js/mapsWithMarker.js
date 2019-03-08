1fb05bcc9101dfece187ef1b9c85543719ceeaab
Bishoy Farouk Tawfik <bftawfik@gmail.com>
2019-03-08 12:41:41 +0200

    adding multi marker

--- assets/js/mapsWithMarker.js
+++ assets/js/mapsWithMarker.js
@@ -1,31 +1,67 @@
 var map;
 var service;
 var infowindow;
+var requests = [
+  {
+    query: 'Damascus',
+    fields: ['name', 'geometry'],
+  },
+  {
+    query: 'Mogadishu',
+    fields: ['name', 'geometry'],
+  },
+  {
+    query: 'Ibiza',
+    fields: ['name', 'geometry'],
+  },
+  {
+    query: 'Tahrir',
+    fields: ['name', 'geometry'],
+  },
+  {
+    query: 'Nairobi',
+    fields: ['name', 'geometry'],
+  },
+  {
+    query: 'Kathmandu',
+    fields: ['name', 'geometry'],
+  },
+  {
+    query: 'Bernabau',
+    fields: ['name', 'geometry'],
+  },
+  {
+    query: 'Athens',
+    fields: ['name', 'geometry'],
+  },
+  {
+    query: 'Istanbul',
+    fields: ['name', 'geometry'],
+  }
+];
 
 function initMap() {
-  var sydney = new google.maps.LatLng(-33.867, 151.195);
+  var cairo = new google.maps.LatLng(30.0595581,31.2234449);
 
   infowindow = new google.maps.InfoWindow();
 
   map = new google.maps.Map(
-      document.getElementById('map'), {center: sydney, zoom: 15});
-
-  var request = {
-    query: 'Bahtit',
-    fields: ['name', 'geometry'],
-  };
+      document.getElementById('map'), {center: cairo, zoom: 5});
 
   service = new google.maps.places.PlacesService(map);
 
-  service.findPlaceFromQuery(request, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
+  requests.map(request => {
+    service.findPlaceFromQuery(request, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log(results);
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
 
-      map.setCenter(results[0].geometry.location);
-    }
-  });
+        // map.setCenter(results[0].geometry.location);
+      }
+    });
+  })
 }
 
 function createMarker(place) {
